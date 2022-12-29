7143004ca3db6be398574b393f284bd7de098f31
imran <imran40031@gmail.com>
2022-12-30 01:56:30 +0600

    Initial Commit

---
+++ Todo-app/Todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_appApp.swift
+//  Todo-app
+//
+//  Created by md imran on 30/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
